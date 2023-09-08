145d96303fc6afe8af3ca095bc307dcf6d392710
Abe M <abe.malla8@gmail.com>
2023-09-08 01:55:41 -0700

    Update types

--- app/types/cache.py
+++ app/types/cache.py
@@ -1,11 +1,12 @@
-from enum import Enum
+# Don't use enums because usually places where we use these values require
+# string values, not Enum values.
 
 
-class AuthSession(Enum):
+class AuthSession:
     ADDRESS = 'address'
     SESSION_ID = 'session_id'
     NONCE = 'nonce'
 
 
-class UserKey(Enum):
+class UserKey:
     NONCE = 'nonce'
