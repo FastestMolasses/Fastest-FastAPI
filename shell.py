b5e47ca399b08822a2e4e9d4a3b9079ab7d7c536
Abe M <abe.malla8@gmail.com>
2023-09-13 06:03:02 -0700

    Added comment

--- shell.py
+++ shell.py
@@ -1,3 +1,7 @@
+"""
+This file is used to run a shell with all the models and the session
+loaded. This is useful for debugging and testing.
+"""
 # python -i shell.py
 # noqa: F401
 from app.models.mysql import User, UserNotification, Notification
